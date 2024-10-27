5147e76340e01b965f4094a24d9727545bf1558f
miteshp98 <miteshpanchal3d@gmail.com>
2024-10-27 22:49:21 +0530

    initiate

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import PortfolioApp from "./components/portfolioApp";
+import TriggerManager from "./components/scrollAnimation";
+import FormValidation from "./components/formValidate";
+
+const portfolioApp = new PortfolioApp();
+const ScrollTriggerManager = new TriggerManager();
+const formManager = new FormValidation();
